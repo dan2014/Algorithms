cd13e323761ea90b43a1ad309a0ede761c3ea4cd
dan2014 <dmen326@gmail.com>
2019-07-17 19:03:52 -0400

    Still working on stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,13 +3,50 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  prices_len = len(prices)
+  # arr_min = {
+  #             "index":len(prices)-1,
+  #             "value":None
+  #           }
+  # arr_max = {
+  #             "index":len(prices)-1,
+  #             "value":None
+  #           }
+  cache = {
+            "min":{
+                    "index":0,
+                    "value":prices[0]
+                  },
+            "max":{
+                    "index":0,
+                    "value":prices[0]
+                  }
+          }
+  diff = float("-inf")
+
+  for i in range(1,prices_len):
+    if(prices[i-1] - prices[i] > diff and cache["min"]["index"] > i):
+      diff = prices[i-1] - prices[i]
+      cache["min"]["value"], cache["min"]["index"] = prices[i],i
+      cache["max"]["value"], cache["max"]["index"] = prices[i-1],i
+      print("min",cache["min"]["value"])
+    
+    # if(prices[i] < cache["min"]["value"]):
+    #   cache["min"]["value"], cache["min"]["index"] = prices[i],i
+    # if(prices[i] > cache["max"]["value"]):
+    #   cache["max"]["value"], cache["max"]["index"] = prices[i],i
+
+    print("diff",diff)
+
+  print("sub",cache["max"]["value"] - cache["min"]["value"])
+  print(cache["max"]["value"],cache["min"]["value"])
 
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+  # parser = argparse.ArgumentParser(description='Find max profit from prices.')
+  # parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+  # args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+  # print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+  find_max_profit([1050, 270, 1540, 3800, 2])
